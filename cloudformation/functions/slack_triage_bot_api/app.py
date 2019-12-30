mozilla/MozDef-Triage-Bot

commit 5d0ba052156c5613c2500d62059b66ddace9c2ac

Improve exception handling
Deal with exceptions differently depending on if the function was called
by API Gateway or through direct invocation.

Pass SlackExceptions through to the invoker of the function.

cloudformation/functions/slack_triage_bot_api/app.py
@@ -419,8 +419,8 @@ def lambda_handler(event: dict, context: dict) -> dict:
     :return: An AWS API Gateway output dictionary for proxy mode
     """
     logger.debug('event is {}'.format(event))
-    try:
-        if event.get('resource') == '/{proxy+}':
+    if event.get('resource') == '/{proxy+}':
+        try:
             headers = event['headers'] if event['headers'] is not None else {}
             cookie_header = headers.get('Cookie', '')
             referer = headers.get('Referer', '')
@@ -438,23 +438,28 @@ def lambda_handler(event: dict, context: dict) -> dict:
             else:
                 body = {}
             return process_api_call(event, query_string_parameters, body)
-        else:
-            # Not an API Gateway invocation, we'll assume a direct Lambda invocation
+        except Exception as e:
+            logger.error(str(e))
+            logger.error(traceback.format_exc())
+            return {
+                'headers': {'Content-Type': 'text/html'},
+                'statusCode': 500,
+                'body': 'Error'}
+    else:
+        # Not an API Gateway invocation, we'll assume a direct Lambda invocation
+        try:
             if event.get('action') == 'discover-sqs-queue-url':
                 result = CONFIG.queue_url
             else:
-                result = send_message_to_slack(
-                    event.get('identifier'),
-                    event.get('alert'),
-                    event.get('summary'),
-                    event.get('user'),
-                    event.get('identityConfidence')
-                )
-            return {'result': result}
-    except Exception as e:
-        logger.error(str(e))
-        logger.error(traceback.format_exc())
-        return {
-            'headers': {'Content-Type': 'text/html'},
-            'statusCode': 500,
-            'body': 'Error'}
+                try:
+                    result = send_message_to_slack(
+                        event.get('identifier'),
+                        event.get('alert'),
+                        event.get('summary'),
+                        event.get('user'),
+                        event.get('identityConfidence')
+                    )
+                except SlackException as e:
+                    return {'result': e}
+        except Exception as e:
+            return {'result': str(e)}

cloudformation/functions/slack_triage_bot_api/utils.py
@@ -121,7 +121,14 @@ def call_slack(
             response = requests.post(url, data=data, headers=headers)
         response.raise_for_status()
         if not response.json().get('ok'):
-            raise SlackException(response.json().get('error'))
+            raise SlackException(
+                {
+                    'error': response.json().get('error'),
+                    'url': url,
+                    'data': data,
+                    'response': response.json()
+                }
+            )
     except requests.exceptions.RequestException as e:
         logger.error(
             'POST of response to {} failed {} : {} : {} : {}'.format(

commit 74f8d5f9e56916987f5b4fdd135eafdf1a03af2a

Fix exception handling change to pass results on success
Fix the change introduced in 5d0ba052156c5613c2500d62059b66ddace9c2ac which
fails to pass the result back to the caller upon success.

cloudformation/functions/slack_triage_bot_api/app.py
@@ -460,6 +460,7 @@ def lambda_handler(event: dict, context: dict) -> dict:
                         event.get('identityConfidence')
                     )
                 except SlackException as e:
-                    return {'result': e}
+                    result = e
         except Exception as e:
-            return {'result': str(e)}
+            result = str(e)
+        return result